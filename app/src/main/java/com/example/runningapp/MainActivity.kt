29460eca7ae0f9e3fb7ad34720d2208d8b64a25f
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-09-26 22:38:08 +0200

    Added Dagger-Hilt setup

--- app/src/main/java/com/example/runningapp/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runningapp.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+//@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+//    @Inject
+//    lateinit var runDoa: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Log.d("rundoa", "${runDoa.hashCode()}")
     }
 }
---
+++ app/src/main/java/com/example/runningapp/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.runningapp.other
+
+object Constants {
+
+    const val RUNNING_DATABASE_NAME = "Running_db"
+}
